325151872489a218577cff84fa9d187c63cc2352
michellempi <mmercyphe@student.ciputra.ac.id>
2026-06-11 04:26:38 +0700

    oke

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,27 @@ export const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  console.log(`\n--- Incoming Request: ${req.method} ${req.url} ---`);
+  console.log('Headers:', req.headers);
+  console.log('Query:', req.query);
+  console.log('Body:', req.body);
+  
+  const originalSend = res.send;
+  res.send = function (body) {
+    console.log(`--- Outgoing Response: ${res.statusCode} ---`);
+    try {
+      const parsedBody = typeof body === 'string' ? JSON.parse(body) : body;
+      console.log('Body:', parsedBody);
+    } catch(e) {
+      console.log('Body:', body);
+    }
+    console.log('---------------------------------------------------\n');
+    return originalSend.call(this, body);
+  };
+  next();
+});
+
 registerRoutes(app);
 
 app.use((_req, res) => {
--- src/utils/database-util.ts
+++ src/utils/database-util.ts
@@ -1,11 +1,16 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { env } from "./env-util";
+
+const connectionString = env.DATABASE_URL;
 
 declare global {
-  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma = global.prisma ?? new PrismaClient();
+const adapter = new PrismaPg({ connectionString });
+
+export const prisma = global.prisma ?? new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") {
   global.prisma = prisma;
